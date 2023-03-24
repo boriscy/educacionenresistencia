1acc9889c51c11dbb8337b4d236b0254e97f5f49
Boris Barroso <boriscyber@gmail.com>
2023-03-24 08:57:16 -0400

    Primer commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import "../app.css"
+	//import { theme } from "$lib/stores"
+</script>
+
+<div class="bg-base-200 p-0 mx-auto main">
+	<nav class="fixed z-100 w-full top-0">
+		<div class="container mx-auto px-6 py-3 flex justify-between items-center">
+			<a class="text-white font-bold text-2xl lg:text-4xl" href="/">
+				<img
+					src="/logo.png"
+					width="100"
+					title="2006 - Feria Internacional del Libro de São Paulo."
+				/>
+			</a>
+			<div class="flex items-center">
+				<a class="text-white font-bold mx-3 hover:text-gray-100" href="/"> Inicio </a>
+				<a class="text-white font-bold mx-3 hover:text-gray-100" href="/products"> Productos </a>
+				<a class="text-white font-bold mx-3 hover:text-gray-100" href="/about"> Nosotras </a>
+			</div>
+		</div>
+	</nav>
+
+	<main class="mt-20 mx-auto">
+		<slot />
+	</main>
+</div>
+
+<style>
+	main {
+		@apply max-w-2xl text-lg;
+	}
+	nav {
+		background: #425f5a;
+	}
+	nav > div {
+		@apply max-w-2xl;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl">Educación en Resistencia</h1>
+
+<div class="">
+	<img src="/ale-firmando.jpg" class="mr-2 w-3/5 float-left" />
+
+	<p>
+		El proyecto de la Editorial Educación en Resistencia tiene su germen en el trabajo de correctora
+		de estilo que comencé realizando de manera autodidacta el año 2003. Fui seleccionada a partir de
+		un examen de competencias, que en realidad no era más que la expresión del detallismo y la
+		obsesión por la corrección de estilo. Había que argumentar cada coma, cada punto... Lo hice y me
+		contrataron en la Editorial Brasbol, de São Paulo, Brasil. Comencé también independientemente
+		haciendo el trabajo de corrección de tesis, de distintas universidades. Estas actividades
+		laborales me ayudaron a costear mis estudios en Comunicación Social y en Filosofía y Letras.
+		Contrariamente a lo que se esperaba de esta formación profesional, he logrado levantar este
+		emprendimiento muchos años después de haber comenzado en el área, pero con la misma ilusión en
+		el alma de hacer lo que me apasiona y amo. De esta manera, el nombre de la editorial Educación
+		en Resistencia expresa lo que es y ha sido para mí la labor con la palabra. Contra viento y
+		marea, he concluido mis estudios de ambas licenciaturas, y ahora me encuentro realizando un
+		Máster en Diseño Editorial y Publicaciones Digitales.
+	</p>
+
+	<p class="mt-8">
+		Esta crónica del proyecto deviene en una hoja de vida, pero que en esencia no se dirige a
+		ensalzar mi ego, sino a dejar constancia de que pude contar esta historia, a pesar de todo,
+		resistiendo ante todo. No ha sido un camino solamente sembrado de flores. No detallaré las
+		dificultades, pero las personas que han visto cómo se ha levantado esto desde abajo, sin ningún
+		capital semilla, saben que ésta es una quijotada que al menos vale la pena contar. Y es que me
+		llevó 14 años concluir la carrera de Filosofía y Letras, y desde que inicié estuve publicando.
+		Primero fue una revista llamada Cien de Cien, que alcanzó a 13 números, que salían uno cada año,
+		de manera autogestionaria.
+	</p>
+
+	<p class="mt-8">
+		De ese modo comencé a involucrarme desde un principio en lo que ahora es nuestro fuerte como
+		editorial: la corrección de estilo y la publicación física de textos. Mi producción como poeta
+		también me llevó a practicar por años la autoedición, lo cual me ayudó a experimentar con
+		materiales, técnicas y software distintos. Me animé a ser difusora de literatura científica,
+		histórica, infantil y universal. Comencé imprimiendo en una Canon, ahora tengo cinco Epson, que
+		son como los cinco dedos de una mano. Me he sentido algo cyborg por el tiempo prolongado que he
+		pasado conectada a la computadora y las impresoras. Luego, mi identidad de artesana se ha
+		fortalecido por el trabajo de empastado artesanal que hasta ahora continúo haciendo en los
+		libros que produzco.
+	</p>
+
+	<p class="mt-8">
+		Siento que me desprendo de esta historia para contar otras desde ahora. Siento la necesidad de
+		devolver la confianza que han puesto en mí todos los autores que han publicado conmigo. Este
+		fanzine, que comienza contando la historia de una niña lectora empedernida que se hizo editora,
+		se desarrolla presentando un catálogo particular. Además de encontrar los detalles de los
+		libros,
+	</p>
+</div>
