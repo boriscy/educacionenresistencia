ef7dc61808fb750f1f31d80b76a1751cd150f5de
Boris Barroso <boriscyber@gmail.com>
2023-03-24 10:30:16 -0400

    Config for deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,11 @@
 <div class="bg-base-200 p-0 mx-auto main">
   <nav class="fixed z-100 w-full top-0">
     <div class="container mx-auto px-6 py-3 flex justify-between items-center">
-      <a class="text-white font-bold text-2xl lg:text-4xl" href={base}>
+      <a class="text-white font-bold text-2xl lg:text-4xl" href="{base}/">
         <img src="images/logo.png" width="100" alt="Logo" />
       </a>
       <div class="flex items-center">
-        <a class="text-white font-bold mx-3 hover:text-gray-100" href={base}> Inicio </a>
+        <a class="text-white font-bold mx-3 hover:text-gray-100" href="{base}/"> Inicio </a>
         <a class="text-white font-bold mx-3 hover:text-gray-100" href="{base}products">
           Productos
         </a>
--- svelte.config.js
+++ svelte.config.js
@@ -11,17 +11,10 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      precompress: false,
-      strict: true,
-      default: true,
-      trailingSlash: "always",
-      fallback: "200.html"
-    }),
-    base: process.env.NODE_ENV === "production" ? "/educacionenresistencia" : ""
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/educacionenresistencia" : ""
+    }
   },
   preprocess: vitePreprocess(),
   files: {
